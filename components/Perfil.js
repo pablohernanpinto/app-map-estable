d8e097b8901232be3dc0ac0e663b4cbc6c0ca94e
Pablo Pinto <pablohernanpinto@gmail.com>
2024-12-12 22:06:34 -0300

    ultima modificacion

--- components/Perfil.js
+++ components/Perfil.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
@@ -10,53 +10,120 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Perfil({ route }) {
   const { markers } = route.params || []; // Recuperar los pines desde los parámetros de navegación
 
-  const [isModalVisible, setModalVisible] = useState(false); // Estado del modal
+  const [isModalVisible, setModalVisible] = useState(false); // Estado del modal de sesión
+  const [isEditModalVisible, setEditModalVisible] = useState(false); // Estado del nuevo modal para editar
   const [username, setUsername] = useState(''); // Estado del campo usuario
   const [password, setPassword] = useState(''); // Estado del campo contraseña
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [id, setID] = useState('');
+  const [newNombre, setNewNombre] = useState(''); // Estado para el nuevo nombre
+  const [newCorreo, setNewCorreo] = useState(''); // Estado para el nuevo correo
+  const [incidentes, setIncidentes] = useState([]);  // Estado para almacenar los incidentes
+
+
+  const fetchIncidentes = async () => {
+    console.log('http://192.168.0.12:3000/incidentes/usuario/'+id)
+    try {
+      const response = await axios.get('http://192.168.0.12:3000/incidentes/usuario/'+id);
+      setIncidentes(response.data);  // Almacena los incidentes en el estado
+      console.log(incidentes, 'prueba')
+    } catch (error) {
+      console.error('Error al obtener los incidentes:', error);
+    }
+
+  }
+  // Guardar datos
+  const saveData = async (nombre, correo, id_usuario) => {
+    try {
+      console.log('Guardando datos');
+      await AsyncStorage.setItem('nombre', nombre);
+      await AsyncStorage.setItem('correo', correo);
+      await AsyncStorage.setItem('id_usuario', id_usuario.toString());
+      readData();
+    } catch (e) {
+      console.error('Error al guardar', e);
+    }
+  };
+
+  // Leer datos
+  const readData = async () => {
+    setNombre(await AsyncStorage.getItem('nombre'));
+    setCorreo(await AsyncStorage.getItem('correo'));
+    setID(await AsyncStorage.getItem('id_usuario'));
+  };
 
   const handleLogin = async () => {
+    console.log('1 - Inicio de handleLogin');
+
     const loginData = {
       correo: username,
       password: password,
     };
+    console.log('Datos de login:', loginData);
 
     try {
-      const response = await fetch('http://localhost:3000/usuarios/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData),
-      });
+      console.log('2 - Iniciando solicitud con Axios...');
+      const response = await axios.post('http://192.168.0.12:3000/usuarios/login', loginData);
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Respuesta del servidor:', data);
-        Alert.alert('Éxito', 'Inicio de sesión exitoso');
-      } else {
-        const errorData = await response.json();
-        console.error('Error en la respuesta:', errorData);
-        Alert.alert('Error', errorData.message || 'Error al iniciar sesión.');
-      }
+      console.log('3 - Respuesta del servidor:', response.data);
+      saveData(response.data.nombre, response.data.correo, response.data.id_usuario);
+      Alert.alert('Éxito', 'Inicio de sesión exitoso');
+      fetchIncidentes()
     } catch (error) {
-      console.error('Error al realizar la solicitud:', error);
-      Alert.alert(
-        'Error',
-        'No se pudo conectar con el servidor. Verifica tu conexión.'
-      );
+      console.log('4 - Error al realizar la solicitud con Axios');
+      if (error.response) {
+        console.error('Error en la respuesta del servidor:', error.response.data);
+        Alert.alert('Error', error.response.data.message || 'Error al iniciar sesión.');
+      } else if (error.request) {
+        console.error('No se recibió respuesta del servidor:', error.request);
+        Alert.alert('Error', 'No se pudo conectar con el servidor. Verifica tu conexión.');
+      } else {
+        console.error('Error al configurar la solicitud:', error.message);
+        Alert.alert('Error', 'Ocurrió un error inesperado.');
+      }
     }
 
+    console.log('5 - Finalizando handleLogin');
     setModalVisible(false); // Cierra el modal
+  };
+
+  // Función para actualizar los datos
+  const handleUpdate = async () => {
+    if (!newNombre && !newCorreo) {
+      Alert.alert('Error', 'Por favor, ingresa un nombre o un correo.');
+      return;
+    }
+
+    const updateData = {
+      nombre: newNombre || nombre,
+      correo: newCorreo || correo,
+    };
+    console.log(id)
+    try {
+      console.log('Actualizando datos...');
+      const response = await axios.put(`http://192.168.0.12:3000/usuarios/${id}`, updateData);
+      console.log('Respuesta del servidor:', response.data);
+      saveData(response.data.nombre, response.data.correo, response.data.id_usuario);
+      Alert.alert('Éxito', 'Datos actualizados correctamente');
+      setEditModalVisible(false); // Cierra el modal de edición
+    } catch (error) {
+      console.log('Error al realizar la actualización');
+      Alert.alert('Error', 'No se pudo actualizar la información.');
+    }
   };
 
   return (
     <View style={styles.container}>
-      {/* Botón para abrir el modal */}
+      {/* Botón para abrir el modal de sesión */}
       <Button title="Iniciar sesión" onPress={() => setModalVisible(true)} />
+      <Button title="Cerrar sesión" onPress={() => saveData('', '', '')} />
 
       {/* Modal para iniciar sesión */}
       <Modal
@@ -68,7 +135,6 @@ export default function Perfil({ route }) {
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Iniciar Sesión</Text>
-
             <TextInput
               style={styles.input}
               placeholder="Usuario"
@@ -82,7 +148,6 @@ export default function Perfil({ route }) {
               value={password}
               onChangeText={setPassword}
             />
-
             <View style={styles.modalButtons}>
               <TouchableOpacity
                 style={[styles.button, styles.buttonClose]}
@@ -101,23 +166,67 @@ export default function Perfil({ route }) {
         </View>
       </Modal>
 
-      <Text style={styles.title}>Nombre:</Text>
-      <Text style={styles.title}>Correo:</Text>
-      <Button title="Editar información personal" />
-
-      <Text style={styles.title}>Reportes</Text>
-      <FlatList
-        data={markers}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.markerItem}>
-            <Text>
-              Ubicación: {item.latitude}, {item.longitude}
-            </Text>
-            <Text>Tipo: {item.option}</Text>
+      {/* Modal para editar información personal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isEditModalVisible}
+        onRequestClose={() => setEditModalVisible(false)} // Permite cerrar el modal con el botón de atrás
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Editar Información</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nuevo nombre"
+              value={newNombre}
+              onChangeText={setNewNombre}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Nuevo correo"
+              value={newCorreo}
+              onChangeText={setNewCorreo}
+            />
+            <View style={styles.modalButtons}>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setEditModalVisible(false)}
+              >
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonLogin]}
+                onPress={handleUpdate}
+              >
+                <Text style={styles.buttonText}>Actualizar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        )}
-      />
+        </View>
+      </Modal>
+
+      <Text style={styles.title}>Nombre: {nombre}</Text>
+      <Text style={styles.title}>Correo: {correo}</Text>
+      <Button title="Editar información personal" onPress={() => setEditModalVisible(true)} />
+
+      <View style={styles.section}>
+  <Text style={styles.title}>Reportes</Text>
+  <FlatList
+    data={incidentes}
+    keyExtractor={(item) => item.id_incidente.toString()}
+    renderItem={({ item }) => (
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Tipo: {item.tipo}</Text>
+        <Text>Descripción: {item.descripcion}</Text>
+        <Text>Hora: {item.hora}</Text>
+        <Text>Fecha: {new Date(item.fecha).toLocaleDateString()}</Text>
+        <Text>Latitud: {item.latitude}</Text>
+        <Text>Longitud: {item.longitude}</Text>
+      </View>
+    )}
+  />
+</View>
     </View>
   );
 }
@@ -187,4 +296,29 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
+  section: {
+    marginTop: 20,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  card: {
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    elevation: 2, // Sombra para Android
+    shadowColor: '#000', // Sombra para iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  
 });
--- components/mapScreen.js
+++ components/mapScreen.js
@@ -1,87 +1,159 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Modal, Button, Text, TextInput, Alert } from 'react-native';
+import { StyleSheet, View, Modal, Button, Text, TextInput, Alert, DevSettings } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useNavigation } from '@react-navigation/native';
 import Checkbox from 'expo-checkbox';
 import * as Location from 'expo-location';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 export default function MapScreen() {
   const [region, setRegion] = useState({
     latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    longitude: -122.324,
+    latitudeDelta: 0.005 ,
+    longitudeDelta: 0.005,
   });
-  const [marker, setMarker] = useState([]); // Almacenamiento de múltiples pines
+  const [incidentes, setIncidentes] = useState([]); // Estado para almacenar los incidentes
   const [isModalVisible, setModalVisible] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState(null); // Para almacenar las coordenadas
+  const [selectedIncident, setSelectedIncident] = useState(null); // Para almacenar el incidente seleccionado
+  const [description, setDescription] = useState('');
+  const [nombreAccidente, setNombreAccidente] = useState('');
+  const [id, setID] = useState(null);
   const navigation = useNavigation();
-  const [selectedCheckbox, setSelectedCheckbox] = useState(null); // Estado para el checkbox seleccionado
-  const [description, setDescription] = useState(''); // Para capturar la descripción
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        Alert.alert('Permisos denegados', 'No se puede acceder a la ubicación.');
-        return;
-      }
+    const fetchIncidentes = async () => {
+      try {
+        const response = await axios.get('http://192.168.0.12:3000/incidentes/');
 
-      let location = await Location.getCurrentPositionAsync({});
-      const { latitude, longitude } = location.coords;
-      setRegion({
-        latitude,
-        longitude,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      });
+        setIncidentes(response.data);
+        
+      } catch (err) {
+        console.error('Error al cargar incidentes:', err);
+        Alert.alert('Error', 'No se pudieron cargar los incidentes');
+      } 
+    };
+
+    fetchIncidentes();
+
+    (async () => {
+      try {
+        const userId = await AsyncStorage.getItem('id_usuario');
+        setID(userId);
+
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          Alert.alert('Permisos denegados', 'No se puede acceder a la ubicación.');
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        setRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.0005,
+          longitudeDelta: 0.001,
+        });
+        console.log('aqui')
+      } catch (error) {
+        console.error('Error al obtener ubicación:', error);
+        Alert.alert('Error', 'No se pudo obtener la ubicación.');
+      }
     })();
   }, []);
-
+  const forzarRefresco = () => {
+    setRefresh(true); // Esto activará el efecto
+  };
   const handleMapPress = (e) => {
     const { coordinate } = e.nativeEvent;
-    setSelectedLocation(coordinate); // Guardar la ubicación seleccionada
-    setModalVisible(true); // Mostrar el modal con las opciones
+    setSelectedLocation(coordinate);
+    setModalVisible(true);
   };
 
-  const handleFormSubmit = () => {
+  const handleMarkerPress = (incident) => {
+    setSelectedIncident(incident); // Guardar el incidente seleccionado
+    setModalVisible(true); // Mostrar el modal con los detalles
+  };
+
+  const handleFormSubmit = async () => {
+    const now = new Date();
+    const date = now.toLocaleDateString(); // Fecha en formato local
+    const time = String(now.getHours()).padStart(2, '0') + ':' +
+                String(now.getMinutes()).padStart(2, '0') + ':' +
+                String(now.getSeconds()).padStart(2, '0');
+
     if (selectedLocation) {
       const formData = {
-        selectedOption: selectedCheckbox,
-        description: description,
-        latitude: selectedLocation.latitude, // Guardar la latitud
-        longitude: selectedLocation.longitude, // Guardar la longitud
+        id_usuario: id,
+        fecha: date,
+        hora: time,
+        tipo: nombreAccidente,
+        descripcion: description,
+        latitude: selectedLocation.latitude,
+        longitude: selectedLocation.longitude,
       };
-      console.log('Datos del formulario:', formData);
-      setMarker([...marker, selectedLocation]); // Agregar marcador al mapa
-      setModalVisible(false); // Cerrar el modal
-
-      // Aquí podrías enviar formData a un servidor o hacer lo que necesites
+      try {
+        await axios.post('http://192.168.0.12:3000/incidentes/', formData, {
+          headers: { 'Content-Type': 'application/json' },
+        });
+        Alert.alert('Incidente Registrado', 'El incidente ha sido registrado correctamente');
+      } catch (error) {
+        console.error('Error al registrar incidente:', error);
+        Alert.alert('Error', 'Hubo un error al registrar el incidente');
+      }
     } else {
-      alert('Por favor selecciona una ubicación en el mapa.');
+      Alert.alert('Error', 'Por favor selecciona una ubicación en el mapa.');
     }
   };
 
   const handleCheckboxSelect = (index) => {
-    setSelectedCheckbox(selectedCheckbox === index ? null : index); // Selecciona o desmarca el checkbox
+    switch (index) {
+      case 0:
+        setNombreAccidente('Accidente - Accidente de tránsito');
+        break;
+      case 1:
+        setNombreAccidente('Accidente - Incendio');
+        break;
+      case 2:
+        setNombreAccidente('Delito - Robo/Hurto');
+        break;
+      case 3:
+        setNombreAccidente('Delito - Asalto');
+        break;
+      default:
+        setNombreAccidente('');
+    }
   };
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        onPress={handleMapPress}
         region={region}
+        onPress={handleMapPress}
       >
-        {marker.map((m, index) => (
-          <Marker
-            key={index}
-            coordinate={m}
-            title={`Marcador ${index + 1}`}
-            description={description}
-          />
-        ))}
+        {incidentes.length > 0 ? (
+          incidentes.map((incident) => (
+            <Marker
+              key={incident.id_incidente}
+              coordinate={{
+                latitude: parseFloat(incident.latitude),
+                longitude: parseFloat(incident.longitude),
+              }}
+              title={incident.tipo}
+              description={incident.descripcion}
+              onPress={() => handleMarkerPress(incident)}
+            />
+          ))
+        ) : (
+          <Text style={styles.noIncidentsText}>No hay incidentes disponibles.</Text>
+        )}
       </MapView>
+
       <Modal
         visible={isModalVisible}
         animationType="slide"
@@ -90,52 +162,62 @@ export default function MapScreen() {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text style={styles.titleText}>Opciones para la ubicación seleccionada:</Text>
+            {selectedIncident ? (
+              <>
+                <Text style={styles.titleText}>Detalles del incidente</Text>
+                <Text>Tipo: {selectedIncident.tipo}</Text>
+                <Text>Descripción: {selectedIncident.descripcion}</Text>
+              </>
+            ) : (
+              <>
+                <Text style={styles.titleText}>Opciones para la ubicación seleccionada:</Text>
 
-            <View style={styles.containerCheckBox}>
-              <Checkbox
-                value={selectedCheckbox === 0}
-                onValueChange={() => handleCheckboxSelect(0)}
-              />
-              <Text style={styles.textCheckbox}>Accidente - Accidente de tránsito</Text>
-            </View>
+                <View style={styles.containerCheckBox}>
+                  <Checkbox
+                    value={nombreAccidente === 'Accidente - Accidente de tránsito'}
+                    onValueChange={() => handleCheckboxSelect(0)}
+                  />
+                  <Text style={styles.textCheckbox}>Accidente - Accidente de tránsito</Text>
+                </View>
 
-            <View style={styles.containerCheckBox}>
-              <Checkbox
-                value={selectedCheckbox === 1}
-                onValueChange={() => handleCheckboxSelect(1)}
-              />
-              <Text style={styles.textCheckbox}>Accidente - Incendio</Text>
-            </View>
+                <View style={styles.containerCheckBox}>
+                  <Checkbox
+                    value={nombreAccidente === 'Accidente - Incendio'}
+                    onValueChange={() => handleCheckboxSelect(1)}
+                  />
+                  <Text style={styles.textCheckbox}>Accidente - Incendio</Text>
+                </View>
 
-            <View style={styles.containerCheckBox}>
-              <Checkbox
-                value={selectedCheckbox === 2}
-                onValueChange={() => handleCheckboxSelect(2)}
-              />
-              <Text style={styles.textCheckbox}>Delito - Robo/Hurto</Text>
-            </View>
+                <View style={styles.containerCheckBox}>
+                  <Checkbox
+                    value={nombreAccidente === 'Delito - Robo/Hurto'}
+                    onValueChange={() => handleCheckboxSelect(2)}
+                  />
+                  <Text style={styles.textCheckbox}>Delito - Robo/Hurto</Text>
+                </View>
 
-            <View style={styles.containerCheckBox}>
-              <Checkbox
-                value={selectedCheckbox === 3}
-                onValueChange={() => handleCheckboxSelect(3)}
-              />
-              <Text style={styles.textCheckbox}>Delito - Asalto</Text>
-            </View>
+                <View style={styles.containerCheckBox}>
+                  <Checkbox
+                    value={nombreAccidente === 'Delito - Asalto'}
+                    onValueChange={() => handleCheckboxSelect(3)}
+                  />
+                  <Text style={styles.textCheckbox}>Delito - Asalto</Text>
+                </View>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Descripción"
-              value={description}
-              onChangeText={setDescription}
-            />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Descripción"
+                  value={description}
+                  onChangeText={setDescription}
+                />
 
-            <View style={styles.buttonContainer}>
-              <Button title="Ingresar" onPress={handleFormSubmit} />
-              <Text></Text>
-              <Button title="Cancelar" onPress={() => setModalVisible(false)} />
-            </View>
+                <View style={styles.buttonContainer}>
+                  <Button title="Ingresar" onPress={handleFormSubmit} />
+                  <Text></Text>
+                  <Button title="Cancelar" onPress={() => setModalVisible(false)} />
+                </View>
+              </>
+            )}
           </View>
         </View>
       </Modal>
@@ -185,8 +267,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: 'row',
-
     width: '100%',
     marginTop: 20,
+  },
+  noIncidentsText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
   },
 });
